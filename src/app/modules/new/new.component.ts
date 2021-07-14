8b4449384d7822df2d22c8edfdd22509f157392e
Gianluca Maziero Machado <gianluca.machado@opus-software.com.br>
2021-07-14 17:29:11 -0300

    Validate create account form

--- src/app/modules/new/new.component.ts
+++ src/app/modules/new/new.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormBuilder, FormControl, FormGroup, Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import faker from 'faker';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-new',
@@ -8,7 +14,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./new.component.scss'],
 })
 export class NewComponent implements OnInit {
-  public newForm: FormGroup;
+  public newForm!: FormGroup;
 
   public param_agreement: any = { url: 'https://www.thecampusqdl.com/uploads/files/pdf_sample_2.pdf' };
 
@@ -18,6 +24,8 @@ export class NewComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
+    private nzMessageService: NzMessageService,
+    private translate: TranslateService,
   ) { }
 
   /**
@@ -25,17 +33,61 @@ export class NewComponent implements OnInit {
    */
   ngOnInit() {
     this.newForm = this.formBuilder.group({
-      name: ['', Validators.compose([Validators.required])],
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      password: ['', Validators.compose([Validators.required])],
-      confirm: ['', Validators.compose([Validators.required])],
-      term: [false, Validators.compose([Validators.required])],
+      name: ['', [Validators.required]],
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      confirm: ['', [Validators.required, this.confirmationValidator]],
+      term: [false],
     });
+
+    if (!environment.production) {
+      const password = faker.internet.password();
+      console.log(password);
+
+      this.newForm.patchValue({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password,
+        confirm: password,
+        term: true,
+      });
+    }
   }
+
+  updateConfirmValidator(): void {
+    Promise.resolve().then(() => this.newForm.controls.confirm.updateValueAndValidity());
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } if (control.value !== this.newForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
 
   submitForm() {
     const value = this.newForm.getRawValue();
     console.log(value);
-    this.router.navigate(['login']);
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const i in this.newForm.controls) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (this.newForm.controls.hasOwnProperty(i)) {
+        this.newForm.controls[i].markAsDirty();
+        this.newForm.controls[i].updateValueAndValidity();
+      }
+    }
+
+    if (this.newForm.valid) {
+      if (!this.newForm.controls.term.value) {
+        this.translate.get('create_account.form.agreement_warning').subscribe((res: string) => {
+          this.nzMessageService.warning(res);
+        });
+      } else {
+        this.router.navigate(['login']);
+      }
+    }
   }
 }
